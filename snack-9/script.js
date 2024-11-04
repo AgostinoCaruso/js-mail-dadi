7fc47ab3a52009f39af35c05add299f114cdb511
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-04 15:28:51 +0100

    snack - 9 finish

--- snack-9/script.js
+++ snack-9/script.js
@@ -6,3 +6,12 @@ Calcola la somma e la media dei primi 10 numeri.
 */
 
 const primiNumeri = [0,1,2,3,4,5,6,7,8,9];
+let sum = 0;
+let med = 0;
+primiNumeri.forEach(element => {
+    sum += element;
+});
+
+med = sum / primiNumeri.length;
+
+console.log("la somma è: " + sum + ", mentre la media è: " +med);
