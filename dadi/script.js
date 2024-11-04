65d17f05c0ae6c9a2bcdb9168a970a00f4202a66
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-04 15:06:10 +0100

    add snack-6

--- dadi/script.js
+++ dadi/script.js
@@ -8,23 +8,23 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 
-const pcNumb = GenerateRndNumb();
-const userNumb = GenerateRndNumb();
+// const pcNumb = GenerateRndNumb();
+// const userNumb = GenerateRndNumb();
 
 
-if(pcNumb > userNumb){
-    //vince pc
-    console.log("Ha vinto il pc con:" + pcNumb + ", contro il: " + userNumb + ", del player");
-}else if(userNumb > pcNumb){
-    //vince player
-    console.log("Ha vinto il player con:" + userNumb + ", contro il: " + pcNumb + ", del pc");
-}else{
-    //patta
-    console.log(`Il risultato è pareggio.(pc:${pcNumb}, user:${userNumb})`);
-}
+// if(pcNumb > userNumb){
+//     //vince pc
+//     console.log("Ha vinto il pc con:" + pcNumb + ", contro il: " + userNumb + ", del player");
+// }else if(userNumb > pcNumb){
+//     //vince player
+//     console.log("Ha vinto il player con:" + userNumb + ", contro il: " + pcNumb + ", del pc");
+// }else{
+//     //patta
+//     console.log(`Il risultato è pareggio.(pc:${pcNumb}, user:${userNumb})`);
+// }
 
-function GenerateRndNumb(){
-    return Math.floor(Math.random()* 6+1);
-}
+// function GenerateRndNumb(){
+//     return Math.floor(Math.random()* 6+1);
+// }
 
 
--- snack-5/script.js
+++ snack-5/script.js
@@ -7,19 +7,19 @@ console.clear();
  se è dispari inseriscilo nell’array
 */
 
-const oddNumb = [];
-let userInput = 0;
-let counter = 1;
-//ask 6 times to enter a numb
-for(let i = 0; i <6; i++){
-    userInput = parseInt(prompt("Scrivi il numero: " + counter));
-    counter++;
-    if(userInput % 2 !=0 && !isNaN(userInput)){
-        oddNumb.push(userInput);
-    }
-}
-if(!!oddNumb.length){
-    console.log(oddNumb.join(" | "));
-}else{
-    console.log("Non hai inserito numeri dispari");
-}
+// const oddNumb = [];
+// let userInput = 0;
+// let counter = 1;
+// //ask 6 times to enter a numb
+// for(let i = 0; i <6; i++){
+//     userInput = parseInt(prompt("Scrivi il numero: " + counter));
+//     counter++;
+//     if(userInput % 2 !=0 && !isNaN(userInput)){
+//         oddNumb.push(userInput);
+//     }
+// }
+// if(!!oddNumb.length){
+//     console.log(oddNumb.join(" | "));
+// }else{
+//     console.log("Non hai inserito numeri dispari");
+// }
