655b407c7ed5d2a82ffd114e74b80c10468c5bb8
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-04 15:51:34 +0100

    snack 6 finish

--- snack-6/script.js
+++ snack-6/script.js
@@ -1,2 +1,19 @@
 "Use Strict";
 console.clear();
+/*
+ Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente
+*/
+
+const numberUser = parseInt(prompt("Scrivi un numero: "));
+let arrayNumberCubici = [];
+if(!isNaN(numberUser)){
+    for (let i = 0; i <= numberUser; i++) {
+        arrayNumberCubici.push(i * i);
+        if(i != 0 && i != 1){
+            console.log(i + "| => al cubo: " + arrayNumberCubici[i])
+        }
+    }
+}else{
+    console.log("non hai inserito un numero.");
+}
