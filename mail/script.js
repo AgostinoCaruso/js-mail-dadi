f22b4535c5981b93bb66a2e53af383dfa9d5cedc
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-04 17:26:36 +0100

    fixd snack 6, calc cubo

--- mail/script.js
+++ mail/script.js
@@ -8,18 +8,18 @@ stampa un messaggio appropriato sull’esito del controllo.
 Non è necessario provvedere alla validazione delle email
 */
 
-// const emailList = ["ciccio", "bubba", "buzzo", "bibbo"];
-// let emailUser = prompt("Scrivi la tua email: ");
-// let isFound = false;
-// for (let i = 0; i < emailList.length; i++) {
-//     console.log(emailUser, emailList[i]);
-//     if (emailUser == emailList[i]) {
-//         isFound = true;
-//         break;
-//     }
-// }
-// if (isFound) {
-//     console.log("L'email inserita è:" + emailUser + ", è un email presente nella lista");
-// } else {
-//     console.log("L'email inserita è:" + emailUser + ", ma non è un email presente nella lista");
-// }
+const emailList = ["ciccio", "bubba", "buzzo", "bibbo"];
+let emailUser = prompt("Scrivi la tua email: ");
+let isFound = false;
+for (let i = 0; i < emailList.length; i++) {
+    console.log(emailUser, emailList[i]);
+    if (emailUser == emailList[i]) {
+        isFound = true;
+        break;
+    }
+}
+if (isFound) {
+    console.log("L'email inserita è:" + emailUser + ", è un email presente nella lista");
+} else {
+    console.log("L'email inserita è:" + emailUser + ", ma non è un email presente nella lista");
+}
--- snack-6/script.js
+++ snack-6/script.js
@@ -9,11 +9,16 @@ const numberUser = parseInt(prompt("Scrivi un numero: "));
 let arrayNumberCubici = [];
 if(!isNaN(numberUser)){
     for (let i = 0; i <= numberUser; i++) {
-        arrayNumberCubici.push(i * i);
-        if(i != 0 && i != 1){
+        arrayNumberCubici.push(NumbCubo(i));
+        if(i != 0 && i != 1){//per non printare 0 e 1
             console.log(i + "| => al cubo: " + arrayNumberCubici[i])
         }
     }
 }else{
     console.log("non hai inserito un numero.");
+}
+
+function NumbCubo(numero){
+    let calcolo = Math.pow(numero,3);
+    return calcolo;
 }
