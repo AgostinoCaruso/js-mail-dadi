6750d49c58c330e8f08984aed9fe62e79698daf4
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-04 14:13:23 +0100

    add email, first var

---
+++ mail/script.js
@@ -0,0 +1,10 @@
+/*
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email
+*/
+
+const emailList = ["ciccio, bubba, buzzo, bibbo"]
+const emailUser = prompt("Scrivi la tua email: ");
