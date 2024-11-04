aed57c01cf9e6d31ab9cebc5977474f108bf7916
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-04 17:54:31 +0100

    add validation mail

--- mail/script.js
+++ mail/script.js
@@ -8,18 +8,26 @@ stampa un messaggio appropriato sull’esito del controllo.
 Non è necessario provvedere alla validazione delle email
 */
 
-const emailList = ["ciccio", "bubba", "buzzo", "bibbo"];
+//contiene la chiocciola? contiene il .com o .it?
+
+const emailList = ["ciccio@gmail.com", "bubba@gmail.com", "buzzo@hotmail.it", "bibbo@hotmail.it"];
 let emailUser = prompt("Scrivi la tua email: ");
 let isFound = false;
-for (let i = 0; i < emailList.length; i++) {
-    console.log(emailUser, emailList[i]);
-    if (emailUser == emailList[i]) {
-        isFound = true;
-        break;
+//controllo prima di iterarla nel for se l'email è idonea e se presenta i caratteri indispensabili
+let isIdoneo = emailUser.includes("@") && (emailUser.includes(".com")) || (emailUser.includes(".it"));
+if (isIdoneo) {//se è idonea
+    for (let i = 0; i < emailList.length; i++) {
+        //console.log(emailUser, emailList[i]);
+        if (emailUser == emailList[i]) {
+            isFound = true;
+            break;//blocco il flusso del for se è stata trovata
+        }
     }
-}
-if (isFound) {
-    console.log("L'email inserita è:" + emailUser + ", è un email presente nella lista");
-} else {
-    console.log("L'email inserita è:" + emailUser + ", ma non è un email presente nella lista");
+    if (isFound) {
+        console.log("L'email inserita è:" + emailUser + ", è un email presente nella lista!");
+    } else {
+        console.log("L'email inserita è:" + emailUser + ", ma non è un email presente nella lista!!");
+    }
+}else{
+    console.log("non hai inserito dell'email valide");
 }
