be94d53f9de340527c734dd4d86c94d089229e97
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-04 15:20:00 +0100

    snack 7 finish

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,2 +1,15 @@
 "Use Strict";
 console.clear();
+
+/*
+Stampa le potenze di 2 fino a 1000
+*/
+
+const exponent = 2;
+let currentNum = 1;
+let lap = 1;
+while(currentNum <1000 && lap <=100){
+    currentNum*=exponent;
+    console.log(currentNum + "| lap: " + lap);
+    lap ++;
+}
